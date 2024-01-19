4b608706191dffeab1d28e48c7df747d2b0b5d5a
rjones18 <regjones16@outlook.com>
2024-01-19 20:57:46 +0000

    Initial commit

---
+++ infra-trigger.tf
@@ -0,0 +1,12 @@
+resource "google_cloudbuild_trigger" "example" {
+  project = "alert-flames-286515"
+  name    = "GKE-Pokemon-App-Trigger"
+  disabled = false
+  service_account = "projects/alert-flames-286515/serviceAccounts/cloudbuildaccount@alert-flames-286515.iam.gserviceaccount.com"
+
+  trigger_template {
+    repo_name   = "github_rjones18_gcp-gke-pokemon-app"
+    branch_name = "main"
+  }
+  filename = "cloudbuild.yaml"
+}
